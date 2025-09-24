db8acef33fd206359d852a330adfd509d92a4770
ZynXx <techigwe@gmail.com>
2025-09-24 21:42:06 +0100

    Create loader.tsx

---
+++ components/loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="w-12 h-12 border-4 border-lycee-purple border-t-lycee-pink rounded-full animate-spin"></div>
+    </div>
+  );
+}
